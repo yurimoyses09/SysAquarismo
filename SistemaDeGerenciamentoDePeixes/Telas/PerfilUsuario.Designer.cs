086ac320d8bb14950a284350b690f4910e356ff1
yurimoyses09 <yuri09.moyses@gmail.com>
2021-06-20 17:49:42 -0300

    Nova tela do perfil de usuario

---
+++ SistemaDeGerenciamentoDePeixes/Telas/PerfilUsuario.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace SistemaDeGerenciamentoDePeixes.Telas
+{
+  partial class PerfilUsuario
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PerfilUsuario));
+            this.btnVoltarTelaInicial = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblNomeLogin = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblPais = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtNomeDeUsuario = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVoltarTelaInicial
+            // 
+            this.btnVoltarTelaInicial.Location = new System.Drawing.Point(13, 13);
+            this.btnVoltarTelaInicial.Name = "btnVoltarTelaInicial";
+            this.btnVoltarTelaInicial.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltarTelaInicial.TabIndex = 0;
+            this.btnVoltarTelaInicial.Text = "Voltar";
+            this.btnVoltarTelaInicial.UseVisualStyleBackColor = true;
+            this.btnVoltarTelaInicial.Click += new System.EventHandler(this.btnVoltarTelaInicial_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(53, 67);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Location = new System.Drawing.Point(53, 100);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(34, 13);
+            this.lblIdade.TabIndex = 2;
+            this.lblIdade.Text = "Idade";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(53, 136);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefone.TabIndex = 3;
+            this.lblTelefone.Text = "Telefone";
+            // 
+            // lblNomeLogin
+            // 
+            this.lblNomeLogin.AutoSize = true;
+            this.lblNomeLogin.Location = new System.Drawing.Point(53, 168);
+            this.lblNomeLogin.Name = "lblNomeLogin";
+            this.lblNomeLogin.Size = new System.Drawing.Size(89, 13);
+            this.lblNomeLogin.TabIndex = 4;
+            this.lblNomeLogin.Text = "Nome de Usuario";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Location = new System.Drawing.Point(53, 198);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(38, 13);
+            this.lblSenha.TabIndex = 5;
+            this.lblSenha.Text = "Senha";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(53, 230);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(31, 13);
+            this.lblSexo.TabIndex = 6;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // lblPais
+            // 
+            this.lblPais.AutoSize = true;
+            this.lblPais.Location = new System.Drawing.Point(53, 257);
+            this.lblPais.Name = "lblPais";
+            this.lblPais.Size = new System.Drawing.Size(27, 13);
+            this.lblPais.TabIndex = 7;
+            this.lblPais.Text = "Pais";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(253, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 8;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(145, 67);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.ReadOnly = true;
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 9;
+            // 
+            // txtNomeDeUsuario
+            // 
+            this.txtNomeDeUsuario.Location = new System.Drawing.Point(145, 165);
+            this.txtNomeDeUsuario.Name = "txtNomeDeUsuario";
+            this.txtNomeDeUsuario.ReadOnly = true;
+            this.txtNomeDeUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtNomeDeUsuario.TabIndex = 10;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(145, 133);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.ReadOnly = true;
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 11;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(145, 97);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.ReadOnly = true;
+            this.txtIdade.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade.TabIndex = 12;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(145, 195);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.ReadOnly = true;
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 13;
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(145, 227);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.ReadOnly = true;
+            this.txtSexo.Size = new System.Drawing.Size(100, 20);
+            this.txtSexo.TabIndex = 14;
+            // 
+            // txtPais
+            // 
+            this.txtPais.Location = new System.Drawing.Point(145, 254);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.ReadOnly = true;
+            this.txtPais.Size = new System.Drawing.Size(100, 20);
+            this.txtPais.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "label1";
+            // 
+            // PerfilUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SistemaDeGerenciamentoDePeixes.Properties.Resources.MED58c45d2587dc28adae469cbfdf573410;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(312, 345);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNomeDeUsuario);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblPais);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblNomeLogin);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnVoltarTelaInicial);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PerfilUsuario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PerfilUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnVoltarTelaInicial;
+    private System.Windows.Forms.Label lblNome;
+    private System.Windows.Forms.Label lblIdade;
+    private System.Windows.Forms.Label lblTelefone;
+    private System.Windows.Forms.Label lblNomeLogin;
+    private System.Windows.Forms.Label lblSenha;
+    private System.Windows.Forms.Label lblSexo;
+    private System.Windows.Forms.Label lblPais;
+    private System.Windows.Forms.Label label8;
+    private System.Windows.Forms.TextBox txtNome;
+    private System.Windows.Forms.TextBox txtNomeDeUsuario;
+    private System.Windows.Forms.TextBox txtTelefone;
+    private System.Windows.Forms.TextBox txtIdade;
+    private System.Windows.Forms.TextBox txtSenha;
+    private System.Windows.Forms.TextBox txtSexo;
+    private System.Windows.Forms.TextBox txtPais;
+    private System.Windows.Forms.Label label1;
+  }
+}
---
+++ SistemaDeGerenciamentoDePeixes/Telas/PerfilUsuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeGerenciamentoDePeixes.Telas
+{
+    public partial class PerfilUsuario : Form
+    {
+        public PerfilUsuario(string name)
+        {
+            InitializeComponent();
+            label1.Text = name;
+        }
+
+        private void btnVoltarTelaInicial_Click(object sender, EventArgs e)
+        {
+            TelaInicial telaInicial = new TelaInicial(label1.Text);
+            this.Visible = false;
+
+            telaInicial.Visible = true;
+
+        }
+    }
+}
